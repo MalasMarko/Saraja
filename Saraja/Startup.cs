d76d29ef748fe6882f0b354383eca8c8b8ef4dc9
Marko Malovic <markosaratoga@gmail.com>
2022-02-23 18:31:18 +0100

    https disable

--- Saraja/Startup.cs
+++ Saraja/Startup.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Policy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
